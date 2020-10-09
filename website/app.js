da65b1c01a629913014b5da6447f35d31d0819c6
tarek <progtarek@gmail.com>
2020-10-10 00:12:56 +0200

    feat add async func and style UI

--- website/app.js
+++ website/app.js
@@ -1,5 +1,21 @@
 /* Global Variables */
+const API_KEY = 'fce79c782428128cb595bec2a1977f31';
+const baseURL = `http://api.openweathermap.org/data/2.5/weather`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+const fetchWeather = async (baseURL, zip, API_KEY) => {
+  const res = await fetch(`${baseURL}?appid=${API_KEY}&zip=${zip}`);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log('Ooops... ', err);
+  }
+};
+
+fetchWeather(baseURL, 94040, API_KEY).then((res) => {
+  console.log(res);
+});
