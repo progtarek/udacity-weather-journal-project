c84791b2e0e0a2466e1b4c71d5b78dcfab541701
tarek <progtarek@gmail.com>
2020-10-10 00:43:44 +0200

    feat: add async to post weather data

--- website/app.js
+++ website/app.js
@@ -2,10 +2,6 @@
 const API_KEY = 'fce79c782428128cb595bec2a1977f31';
 const baseURL = `http://api.openweathermap.org/data/2.5/weather`;
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
-
 const fetchWeather = async (baseURL, zip, API_KEY) => {
   const res = await fetch(`${baseURL}?appid=${API_KEY}&zip=${zip}`);
   try {
@@ -16,9 +12,41 @@ const fetchWeather = async (baseURL, zip, API_KEY) => {
   }
 };
 
+const postData = async (path, data = {}) => {
+  return await fetch(path, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    body: JSON.stringify(data),
+  });
+};
+
+const saveWeatherData = async (temperature) => {
+  let d = new Date();
+  let date = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+  const userResponse = document.getElementById('feelings').value;
+  const res = await postData('/', { temperature, date, userResponse });
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log('Ooops... ', err);
+  }
+};
+
 const onGenerateClickHandler = async () => {
-  const res = await fetchWeather(baseURL, 94040, API_KEY);
-  console.log(res);
+  const zipCode = document.getElementById('zip').value;
+  if (!zipCode.trim()) return;
+  fetchWeather(baseURL, zipCode, API_KEY).then((res) => {
+    saveWeatherData(res.main.temp).then((res) => {
+      console.log('==================================');
+      console.log(res);
+      console.log('==================================');
+    });
+  });
 };
 
 const generateElement = document.getElementById('generate');
